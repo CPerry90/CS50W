58041c9dbd7d2f488d5db1db9c3a4b022e0415aa
Chris Perry <cmperry@live.co.uk>
2022-11-02 17:03:16 +0000

    finishing touches ready to submit

--- auctions/forms.py
+++ auctions/forms.py
@@ -8,7 +8,7 @@ class newListingForm(forms.Form):
     description = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': 'Description of item...', "class": "my-2", "style": "width:100%"}))
     url = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Image URL (optional)', "class": "my-2", "style": "width:100%"}), required=False)
     price = forms.DecimalField(label="", widget=forms.NumberInput(attrs={'placeholder': 'Price...', "class": "my-2", "style": "width:50%"}))
-    category = forms.ModelChoiceField(queryset=category.objects.all(), label="", widget=forms.Select(attrs={"class": "my-2", "style": "width:50%"}), required=False)
+    category = forms.ModelChoiceField(queryset=category.objects.all(), label="", empty_label="Select a Category", widget=forms.Select(attrs={"class": "my-2", "style": "width:50%"}), required=False)
     newCategory = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Or add new category...', "class": "my-2", "style": "width:50%"}), required=False)
 
 class commentForm(forms.Form):
