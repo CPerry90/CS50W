1e89c6284daf6c3496a6fca87ba3d90b47066c9d
Chris Perry <cmperry@live.co.uk>
2022-11-23 16:46:50 +0000

    -Operator mobile styling

--- volunteercenter/webpack.config.js
+++ volunteercenter/webpack.config.js
@@ -3,6 +3,33 @@ const webpack = require("webpack");
 
 module.exports = [
     {
+        entry: "./src/main.js",
+        output: {
+            path: path.resolve(__dirname, "./static/volunteercenter"),
+            filename: "main.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                    },
+                },
+            ],
+        },
+        optimization: {
+            minimize: true,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                // This has effect on the react lib size
+                NODE_ENV: JSON.stringify("production"),
+            }),
+        ],
+    },
+    {
         entry: "./src/callhandler/callhandler.js",
         output: {
             path: path.resolve(__dirname, "./static/volunteercenter"),
